30ee03de7cf2180834b826a7d9e32d263df375e7
TheXeler <1377266215@qq.com>
2026-04-11 01:10:20 +0800

    Fix menus.js

--- menus.js
+++ menus.js
@@ -6,134 +6,134 @@ document.currentScript.outerHTML=`
 <nav>
 	<a href="/modding_guide/index.html">Stonehearth 模组制作指南</a>
 	<section>
-		<a href="/modding_guide/modding_guide/essentials/index.html">模组制作基础</a>
+		<a href="/modding_guide/essentials/index.html">模组制作基础</a>
 		<div>
-			<a href="/modding_guide/modding_guide/essentials/managing/index.html">模组管理</a>
+			<a href="/modding_guide/essentials/managing/index.html">模组管理</a>
 			<div>
-				<a href="/modding_guide/modding_guide/essentials/managing/workshop/index.html">Steam 创意工坊模组</a>
-				<a href="/modding_guide/modding_guide/essentials/managing/unmanaged/index.html">本地模组</a>
+				<a href="/modding_guide/essentials/managing/workshop/index.html">Steam 创意工坊模组</a>
+				<a href="/modding_guide/essentials/managing/unmanaged/index.html">本地模组</a>
 			</div>
-			<a href="/modding_guide/modding_guide/essentials/programming_languages/index.html">编程语言</a>
-			<a href="/modding_guide/modding_guide/essentials/tools/index.html">模组制作工具</a>
-			<a href="/modding_guide/modding_guide/essentials/stonehearth/index.html">Stonehearth 模组</a>
-			<a href="/modding_guide/modding_guide/essentials/json/index.html">JSON 简介</a>
-			<a href="/modding_guide/modding_guide/essentials/manifest/index.html">清单文件（Manifest）</a>
-			<a href="/modding_guide/modding_guide/essentials/entities_components/index.html">实体与组件</a>
-			<a href="/modding_guide/modding_guide/essentials/mixins/index.html">Mixins</a>
-			<a href="/modding_guide/modding_guide/essentials/mixintos_overrides/index.html">Mixintos 和覆盖（Overrides）</a>
-			<a href="/modding_guide/modding_guide/essentials/removing/index.html">Mixintypes</a>
-			<a href="/modding_guide/modding_guide/essentials/localization/index.html">本地化</a>
-			<a href="/modding_guide/modding_guide/essentials/debugging/index.html">错误调试方法</a>
-			<a href="/modding_guide/modding_guide/essentials/debugtools/index.html">使用调试工具</a>
+			<a href="/modding_guide/essentials/programming_languages/index.html">编程语言</a>
+			<a href="/modding_guide/essentials/tools/index.html">模组制作工具</a>
+			<a href="/modding_guide/essentials/stonehearth/index.html">Stonehearth 模组</a>
+			<a href="/modding_guide/essentials/json/index.html">JSON 简介</a>
+			<a href="/modding_guide/essentials/manifest/index.html">清单文件（Manifest）</a>
+			<a href="/modding_guide/essentials/entities_components/index.html">实体与组件</a>
+			<a href="/modding_guide/essentials/mixins/index.html">Mixins</a>
+			<a href="/modding_guide/essentials/mixintos_overrides/index.html">Mixintos 和覆盖（Overrides）</a>
+			<a href="/modding_guide/essentials/removing/index.html">Mixintypes</a>
+			<a href="/modding_guide/essentials/localization/index.html">本地化</a>
+			<a href="/modding_guide/essentials/debugging/index.html">错误调试方法</a>
+			<a href="/modding_guide/essentials/debugtools/index.html">使用调试工具</a>
 			<div>
-				<a href="/modding_guide/modding_guide/essentials/debugtools/debugtools_mod/index.html">debugtools 模组</a>
-				<a href="/modding_guide/modding_guide/essentials/debugtools/default_console/index.html">默认控制台</a>
+				<a href="/modding_guide/essentials/debugtools/debugtools_mod/index.html">debugtools 模组</a>
+				<a href="/modding_guide/essentials/debugtools/default_console/index.html">默认控制台</a>
 			</div>
-			<a href="/modding_guide/modding_guide/essentials/startermods/index.html">入门模组示例</a>
-			<a href="/modding_guide/modding_guide/essentials/shed/index.html">使用 SHED 节省时间</a>
+			<a href="/modding_guide/essentials/startermods/index.html">入门模组示例</a>
+			<a href="/modding_guide/essentials/shed/index.html">使用 SHED 节省时间</a>
 		</div>
-		<a href="/modding_guide/modding_guide/basic/index.html">基础模组制作</a>
+		<a href="/modding_guide/basic/index.html">基础模组制作</a>
 		<div>
-			<a href="/modding_guide/modding_guide/basic/art/index.html">美术工作流程</a>
+			<a href="/modding_guide/basic/art/index.html">美术工作流程</a>
 			<div>
-				<a href="/modding_guide/modding_guide/basic/art/modeling/index.html">创建模型</a>
-				<a href="/modding_guide/modding_guide/basic/art/icons/index.html">创建 2D 资源</a>
-				<a href="/modding_guide/modding_guide/basic/art/vfx/index.html">创建特效</a>
+				<a href="/modding_guide/basic/art/modeling/index.html">创建模型</a>
+				<a href="/modding_guide/basic/art/icons/index.html">创建 2D 资源</a>
+				<a href="/modding_guide/basic/art/vfx/index.html">创建特效</a>
 				<div>
-					<a href="/modding_guide/modding_guide/basic/art/vfx/animations/index.html">动画</a>
+					<a href="/modding_guide/basic/art/vfx/animations/index.html">动画</a>
 					<div>
-						<a href="/modding_guide/modding_guide/basic/art/vfx/animations/anim_existing/index.html">现有实体动画</a>
-						<a href="/modding_guide/modding_guide/basic/art/vfx/animations/anim_custom/index.html">自定义实体动画</a>
+						<a href="/modding_guide/basic/art/vfx/animations/anim_existing/index.html">现有实体动画</a>
+						<a href="/modding_guide/basic/art/vfx/animations/anim_custom/index.html">自定义实体动画</a>
 					</div>
-					<a href="/modding_guide/modding_guide/basic/art/vfx/cubemitters/index.html">粒子效果</a>
-					<a href="/modding_guide/modding_guide/basic/art/vfx/animated_lights/index.html">光照效果</a>
-					<a href="/modding_guide/modding_guide/basic/art/vfx/other_effects/index.html">其他效果</a>
+					<a href="/modding_guide/basic/art/vfx/cubemitters/index.html">粒子效果</a>
+					<a href="/modding_guide/basic/art/vfx/animated_lights/index.html">光照效果</a>
+					<a href="/modding_guide/basic/art/vfx/other_effects/index.html">其他效果</a>
 				</div>
 			</div>
-			<a href="/modding_guide/modding_guide/basic/adding_items/index.html">添加新物品</a>
+			<a href="/modding_guide/basic/adding_items/index.html">添加新物品</a>
 			<div>
-				<a href="/modding_guide/modding_guide/basic/adding_items/entity_forms/index.html">实体表单（Entity Forms）</a>
-				<a href="/modding_guide/modding_guide/basic/adding_items/cloning_manually/index.html">手动添加物品</a>
-				<a href="/modding_guide/modding_guide/basic/adding_items/cloning_with_shed/index.html">使用 SHED 添加物品</a>
-				<a href="/modding_guide/modding_guide/basic/adding_items/item_scale/index.html">修改缩放比例</a>
+				<a href="/modding_guide/basic/adding_items/entity_forms/index.html">实体表单（Entity Forms）</a>
+				<a href="/modding_guide/basic/adding_items/cloning_manually/index.html">手动添加物品</a>
+				<a href="/modding_guide/basic/adding_items/cloning_with_shed/index.html">使用 SHED 添加物品</a>
+				<a href="/modding_guide/basic/adding_items/item_scale/index.html">修改缩放比例</a>
 			</div>
-			<a href="/modding_guide/modding_guide/basic/catalog/index.html">物品目录（Catalog）</a>
-			<a href="/modding_guide/modding_guide/basic/generic_resources/index.html">通用资源</a>
-			<a href="/modding_guide/modding_guide/basic/craftables/index.html">添加配方</a>
-			<a href="/modding_guide/modding_guide/basic/wearables/index.html">添加装备</a>
+			<a href="/modding_guide/basic/catalog/index.html">物品目录（Catalog）</a>
+			<a href="/modding_guide/basic/generic_resources/index.html">通用资源</a>
+			<a href="/modding_guide/basic/craftables/index.html">添加配方</a>
+			<a href="/modding_guide/basic/wearables/index.html">添加装备</a>
 			<div>
-				<a href="/modding_guide/modding_guide/basic/wearables/armor/index.html">护甲和帽子</a>
-				<a href="/modding_guide/modding_guide/basic/wearables/weapons/index.html">武器</a>
+				<a href="/modding_guide/basic/wearables/armor/index.html">护甲和帽子</a>
+				<a href="/modding_guide/basic/wearables/weapons/index.html">武器</a>
 			</div>
-			<a href="/modding_guide/modding_guide/basic/storages/index.html">储物物品</a>
-			<a href="/modding_guide/modding_guide/basic/color_maps/index.html">颜色和材质贴图</a>
+			<a href="/modding_guide/basic/storages/index.html">储物物品</a>
+			<a href="/modding_guide/basic/color_maps/index.html">颜色和材质贴图</a>
 			<div>
-				<a href="/modding_guide/modding_guide/basic/color_maps/shaders/index.html">材质和着色器</a>
+				<a href="/modding_guide/basic/color_maps/shaders/index.html">材质和着色器</a>
 			</div>
-			<a href="/modding_guide/modding_guide/basic/customization/index.html">添加自定义选项</a>
-			<a href="/modding_guide/modding_guide/basic/crops/index.html">添加农作物</a>
-			<a href="/modding_guide/modding_guide/basic/renewable_resources/index.html">可再生资源</a>
-			<a href="/modding_guide/modding_guide/basic/plants/index.html">植物和树木</a>
-			<a href="/modding_guide/modding_guide/basic/static_scenarios/index.html">静态场景</a>
+			<a href="/modding_guide/basic/customization/index.html">添加自定义选项</a>
+			<a href="/modding_guide/basic/crops/index.html">添加农作物</a>
+			<a href="/modding_guide/basic/renewable_resources/index.html">可再生资源</a>
+			<a href="/modding_guide/basic/plants/index.html">植物和树木</a>
+			<a href="/modding_guide/basic/static_scenarios/index.html">静态场景</a>
 			<div>
-				<a href="/modding_guide/modding_guide/basic/static_scenarios/ore_veins/index.html">矿脉</a>
-				<a href="/modding_guide/modding_guide/basic/static_scenarios/landmarks/index.html">地标建筑</a>
-				<a href="/modding_guide/modding_guide/basic/static_scenarios/nests_ruins/index.html">生物巢穴和废墟</a>
+				<a href="/modding_guide/basic/static_scenarios/ore_veins/index.html">矿脉</a>
+				<a href="/modding_guide/basic/static_scenarios/landmarks/index.html">地标建筑</a>
+				<a href="/modding_guide/basic/static_scenarios/nests_ruins/index.html">生物巢穴和废墟</a>
 			</div>
-			<a href="/modding_guide/modding_guide/basic/misc_json/index.html">其他 JSON 文件</a>
+			<a href="/modding_guide/basic/misc_json/index.html">其他 JSON 文件</a>
 			<div>
-				<a href="/modding_guide/modding_guide/basic/misc_json/shops/index.html">配置和商店</a>
+				<a href="/modding_guide/basic/misc_json/shops/index.html">配置和商店</a>
 			</div>
-			<a href="/modding_guide/modding_guide/basic/localization_mod/index.html">翻译模组</a>
+			<a href="/modding_guide/basic/localization_mod/index.html">翻译模组</a>
 		</div>
-		<a href="/modding_guide/modding_guide/intermediate/index.html">中级模组制作</a>
+		<a href="/modding_guide/intermediate/index.html">中级模组制作</a>
 		<div>
-			<a href="/modding_guide/modding_guide/intermediate/regions/index.html">碰撞区域</a>
+			<a href="/modding_guide/intermediate/regions/index.html">碰撞区域</a>
 			<div>
-				<a href="/modding_guide/modding_guide/intermediate/regions/origins/index.html">原点（Origins）</a>
-				<a href="/modding_guide/modding_guide/intermediate/regions/collision_destination/index.html">碰撞和目标点</a>
+				<a href="/modding_guide/intermediate/regions/origins/index.html">原点（Origins）</a>
+				<a href="/modding_guide/intermediate/regions/collision_destination/index.html">碰撞和目标点</a>
 			</div>
-			<a href="/modding_guide/modding_guide/intermediate/building_parts/index.html">建筑部件</a>
-			<a href="/modding_guide/modding_guide/intermediate/commands/index.html">添加命令</a>
-			<a href="/modding_guide/modding_guide/intermediate/buffs/index.html">增益效果（Buffs）</a>
-			<a href="/modding_guide/modding_guide/intermediate/thoughts/index.html">想法（Thoughts）</a>
-			<a href="/modding_guide/modding_guide/intermediate/traits/index.html">特性（Traits）</a>
-			<a href="/modding_guide/modding_guide/intermediate/biomes/index.html">生物群系</a>
+			<a href="/modding_guide/intermediate/building_parts/index.html">建筑部件</a>
+			<a href="/modding_guide/intermediate/commands/index.html">添加命令</a>
+			<a href="/modding_guide/intermediate/buffs/index.html">增益效果（Buffs）</a>
+			<a href="/modding_guide/intermediate/thoughts/index.html">想法（Thoughts）</a>
+			<a href="/modding_guide/intermediate/traits/index.html">特性（Traits）</a>
+			<a href="/modding_guide/intermediate/biomes/index.html">生物群系</a>
 			<div>
-				<a href="/modding_guide/modding_guide/intermediate/biomes/seasons_weather/index.html">季节和天气</a>
+				<a href="/modding_guide/intermediate/biomes/seasons_weather/index.html">季节和天气</a>
 			</div>
-			<a href="/modding_guide/modding_guide/intermediate/jobs/index.html">添加职业</a>
+			<a href="/modding_guide/intermediate/jobs/index.html">添加职业</a>
 			<div>
-				<a href="/modding_guide/modding_guide/intermediate/jobs/crafting_jobs/index.html">制作类职业</a>
-				<a href="/modding_guide/modding_guide/intermediate/jobs/combat_jobs/index.html">战斗类职业</a>
+				<a href="/modding_guide/intermediate/jobs/crafting_jobs/index.html">制作类职业</a>
+				<a href="/modding_guide/intermediate/jobs/combat_jobs/index.html">战斗类职业</a>
 			</div>
-			<a href="/modding_guide/modding_guide/intermediate/kingdoms/index.html">王国</a>
-			<a href="/modding_guide/modding_guide/intermediate/monsters/index.html">生物和怪物</a>
-			<a href="/modding_guide/modding_guide/intermediate/campaigns/index.html">添加战役</a>
+			<a href="/modding_guide/intermediate/kingdoms/index.html">王国</a>
+			<a href="/modding_guide/intermediate/monsters/index.html">生物和怪物</a>
+			<a href="/modding_guide/intermediate/campaigns/index.html">添加战役</a>
 			<div>
-				<a href="/modding_guide/modding_guide/intermediate/campaigns/encounters/index.html">通用遭遇战</a>
+				<a href="/modding_guide/intermediate/campaigns/encounters/index.html">通用遭遇战</a>
 			</div>
-			<a href="/modding_guide/modding_guide/intermediate/hotloading/index.html">热加载模组</a>
-			<a href="/modding_guide/modding_guide/intermediate/testing/index.html">测试你的模组</a>
+			<a href="/modding_guide/intermediate/hotloading/index.html">热加载模组</a>
+			<a href="/modding_guide/intermediate/testing/index.html">测试你的模组</a>
 		</div>
-		<a href="/modding_guide/modding_guide/advanced/index.html">高级模组制作</a>
+		<a href="/modding_guide/advanced/index.html">高级模组制作</a>
 		<div>
-			<a href="/modding_guide/modding_guide/advanced/logging/index.html">用户设置和日志记录</a>
-			<a href="/modding_guide/modding_guide/advanced/events/index.html">事件（Events）</a>
-			<a href="/modding_guide/modding_guide/advanced/components_controllers/index.html">Lua 脚本</a>
+			<a href="/modding_guide/advanced/logging/index.html">用户设置和日志记录</a>
+			<a href="/modding_guide/advanced/events/index.html">事件（Events）</a>
+			<a href="/modding_guide/advanced/components_controllers/index.html">Lua 脚本</a>
 			<div>
-				<a href="/modding_guide/modding_guide/advanced/components_controllers/components_renderers/index.html">组件（Components）</a>
-				<a href="/modding_guide/modding_guide/advanced/components_controllers/controllers/index.html">控制器（Controllers）</a>
+				<a href="/modding_guide/advanced/components_controllers/components_renderers/index.html">组件（Components）</a>
+				<a href="/modding_guide/advanced/components_controllers/controllers/index.html">控制器（Controllers）</a>
 			</div>
-			<a href="/modding_guide/modding_guide/advanced/ai/index.html">AI 行为</a>
-			<a href="/modding_guide/modding_guide/advanced/services/index.html">服务（Services）</a>
-			<a href="/modding_guide/modding_guide/advanced/call_handlers/index.html">调用处理器（Call Handlers）</a>
-			<a href="/modding_guide/modding_guide/advanced/ui/index.html">用户界面（UI）</a>
-			<a href="/modding_guide/modding_guide/advanced/cpp/index.html">C++ 函数</a>
+			<a href="/modding_guide/advanced/ai/index.html">AI 行为</a>
+			<a href="/modding_guide/advanced/services/index.html">服务（Services）</a>
+			<a href="/modding_guide/advanced/call_handlers/index.html">调用处理器（Call Handlers）</a>
+			<a href="/modding_guide/advanced/ui/index.html">用户界面（UI）</a>
+			<a href="/modding_guide/advanced/cpp/index.html">C++ 函数</a>
 		</div>
-		<a href="/modding_guide/modding_guide/ace/index.html">ACE 模组制作</a>
+		<a href="/modding_guide/ace/index.html">ACE 模组制作</a>
 		<div>
-			<a href="/modding_guide/modding_guide/ace/vegetables/index.html">蔬菜</a>
+			<a href="/modding_guide/ace/vegetables/index.html">蔬菜</a>
 		</div>
 	</section>
 </nav>
